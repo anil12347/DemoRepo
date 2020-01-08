377df07f51e3414c9c55e43f2a98ded6deb61cd2
anil12347 <ailds02@gmail.com>
2020-01-08 09:36:35 +0530

    Login page and login page

---
+++ src/com/java/examples/LoginPage.java
@@ -0,0 +1,9 @@
+package com.java.examples;
+
+public class LoginPage {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Login page");
+	}
+
+}
---
+++ src/com/java/examples/SearchPage.java
@@ -0,0 +1,10 @@
+package com.java.examples;
+
+public class SearchPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Search page");
+	}
+
+}
